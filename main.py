b6e9687521e29b631ee63cc2ad3cacb4c39488c4
mleonsarkis <marcoleonsarkis@hotmail.com>
2025-03-01 18:46:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,5 @@ async def teams_webhook(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3978)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    
