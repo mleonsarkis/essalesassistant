8e8f76f2c61aa623c1043b7862bea79d39d7631d
mleonsarkis <marcoleonsarkis@hotmail.com>
2025-03-04 07:53:27 -0600

    Delete data directory

--- data/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MICROSOFT_APP_ID = os.getenv("MICROSOFT_APP_ID", "")
-MICROSOFT_APP_PASSWORD = os.getenv("MICROSOFT_APP_PASSWORD", "")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
