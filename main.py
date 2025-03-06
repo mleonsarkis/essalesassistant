3430b6282b4e973f9008b4fdfe2bba670e9209bc
mleonsarkis <marcoleonsarkis@hotmail.com>
2025-03-06 03:03:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-import uvicorn
 from fastapi import FastAPI
-
-import llm.chatbot
-import teams.handler
-from teams.handler import router
+import uvicorn
 
 app = FastAPI()
 
-app.include_router(router)
-
-
 @app.get("/")
-def health_check():
-    return {"message": "Sales Assistant Bot is running!"}
-
+def home():
+    return {"message": "Test Azure FastAPI Deployment"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    """
-    import asyncio
-    async def test_bot():
-        print("ES Sales Assistant local test. Type 'exit' to quit.\n")
-        while True:
-            user_input = input("You: ")
-            if user_input.lower() == "exit":
-                print("Goodbye")
-                break
-            try:
-                response = await llm.chatbot.process_user_query(user_input)
-                print(f"Bot: {response}\n")
-            except Exception as e:
-                print(f"Error: {e}\n")
-    asyncio.run(test_bot())
-    """
+    uvicorn.run(app, host="0.0.0.0", port=8000)
